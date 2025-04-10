8f91d848d58610103a1be2907c3d2fd32aa47fea
joaquincernik <joaquincernik@gmail.com>
2025-04-09 23:29:44 -0300

    in poco mas

---
+++ tp1/js/scriptComprobante.js
@@ -0,0 +1,4 @@
+function handlerButtonRedirect(){
+    window.location.href = 'http://127.0.0.1:5500/views/transferencia.html'
+
+}
--- tp1/js/scriptTransferencia.js
+++ tp1/js/scriptTransferencia.js
@@ -4,12 +4,20 @@ let inputMultiple = document.getElementById('input-transferencia-multiple')
 let showTransferencia = document.getElementById('show-transferencia')
 
 inputUnico.addEventListener('change',function(){
-    buttonTransferencia.removeAttribute('disabled')
     showTransferencia.style.display = 'block'
 
 })
 
 inputMultiple.addEventListener('change',function(){
-    buttonTransferencia.removeAttribute('disabled')
     showTransferencia.style.display = 'block'
+})
+
+showTransferencia.addEventListener('input',()=>{
+    if(showTransferencia.querySelector('input').value > 0){
+    buttonTransferencia.removeAttribute('disabled')
+    }
+    else{
+        buttonTransferencia.setAttribute('disabled' , '')
+    }
+
 })
